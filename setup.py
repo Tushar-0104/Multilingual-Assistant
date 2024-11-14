6dbd06c066710c9ed43bd978061351a5223fa0cc
Tushar-0104 <tusharsharma7024@gmail.com>
2024-11-14 16:41:54 +0530

    commit third

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=find_packages(),
     author="Tushar Sharma",
     author_email="tusharsharma7024@gmail.com",
-    install_requires=["SpeechRecognition","pydub","PyObjC","pipwin","sounddevice","gTTS","google-generativeai","python-dotenv","streamlit","scipy","playsound"]
+    install_requires=["SpeechRecognition","pipwin","sounddevice","gTTS","google-generativeai","python-dotenv","streamlit","scipy"]
 
 )
